193690abbf9481f6b76f8c12bd1b56d448c36a42
Keklik <ahmedyanov.dinis2014@yandex.ru>
2024-07-18 23:51:58 +0500

    java code

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector(".places__list");
+
+function createCard(data, onDelete) {
+  const userTemplate = document.querySelector("#card-template").content;
+  const userElement = userTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardTitle = userElement.querySelector(".card__title");
+  const cardImage = userElement.querySelector(".card__image");
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+
+  const deleteButton = userElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    onDelete(userElement);
+  });
+  return userElement;
+}
+
+function deleteCard(userElement) {
+  userElement.remove();
+}
+
+initialCards.forEach(function (cardData) {
+  const card = createCard(cardData, deleteCard);
+  placesList.appendChild(card);
+});
